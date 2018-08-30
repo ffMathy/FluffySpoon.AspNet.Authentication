d6cc7154685b0be8a60242388da45a8c2f76a2b3
Mathias Lorenzen <mathias.lorenzen@live.com>
2018-08-30 09:42:17 +0200

    better auth.

--- src/FluffySpoon.AspNet.Authentication.Jwt/PreAuthorizationMiddleware.cs
+++ src/FluffySpoon.AspNet.Authentication.Jwt/PreAuthorizationMiddleware.cs
@@ -31,12 +31,11 @@ namespace FluffySpoon.AspNet.Authentication.Jwt
 		{
 			var authorization = context.Request.Headers.SingleOrDefault(x => x.Key == "Authorization").Value.SingleOrDefault();
 
-			Credentials credentials = null;
-
 			const string fluffySpoonAuthorizationPrefix = "FluffySpoon";
 			const string tokenAuthorizationPrefix = "Bearer";
 			if (authorization != null)
 			{
+				var credentials = new Credentials();
 				if (authorization.StartsWith(fluffySpoonAuthorizationPrefix))
 				{
 					try
@@ -54,45 +53,41 @@ namespace FluffySpoon.AspNet.Authentication.Jwt
 						await _next(context);
 						return;
 					}
-				} else if (authorization.StartsWith(tokenAuthorizationPrefix))
+				}
+
+				var claimsResult = await identityResolver.GetClaimsAsync(credentials);
+				if (claimsResult == null)
 				{
 					await _next(context);
 					return;
 				}
+
+				var claims = claimsResult
+				  .Claims
+				  .Select(x => new Claim(
+					x.Key,
+					x.Value))
+				  .ToList();
+				if (claims.Any(x => x.ValueType == ClaimTypes.Role))
+				{
+					throw new InvalidOperationException("Can't provide roles through claims. Use the roles array instead of the claims result.");
+				}
+
+				foreach (var role in claimsResult.Roles)
+				{
+					claims.Add(new Claim(
+					  ClaimTypes.Role,
+					  role));
+				}
+
+				if (credentials != null)
+					claims.Add(new Claim(
+					  JwtRegisteredClaimNames.Sub,
+					  credentials.Username));
+
+				var token = generator.GenerateToken(claims.ToArray());
+				context.Items.Add(Constants.MiddlewareTokenPassingKey, token);
 			}
-
-			var claimsResult = await identityResolver.GetClaimsAsync(credentials);
-			if (claimsResult == null)
-			{
-				await _next(context);
-				return;
-			}
-
-			var claims = claimsResult
-			  .Claims
-			  .Select(x => new Claim(
-				x.Key,
-				x.Value))
-			  .ToList();
-			if (claims.Any(x => x.ValueType == ClaimTypes.Role))
-			{
-				throw new InvalidOperationException("Can't provide roles through claims. Use the roles array instead of the claims result.");
-			}
-
-			foreach (var role in claimsResult.Roles)
-			{
-				claims.Add(new Claim(
-				  ClaimTypes.Role,
-				  role));
-			}
-
-			if (credentials != null)
-				claims.Add(new Claim(
-				  JwtRegisteredClaimNames.Sub,
-				  credentials.Username));
-
-			var token = generator.GenerateToken(claims.ToArray());
-			context.Items.Add(Constants.MiddlewareTokenPassingKey, token);
 
 			await _next(context);
 		}
